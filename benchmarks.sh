7de8a47287b38042b6c61f067774a13540c10cd5
Leticia Wanderley <leticiafwanderley@gmail.com>
2018-11-25 18:50:28 +0000

    Add AOT option to script

--- benchmarks.sh
+++ benchmarks.sh
@@ -4,16 +4,17 @@ benchmark_path=$2
 output_file=$3
 aot=""
 if [ "$4" = "aot" ]; then
-  aot="-Xshareclasses"
+  aot="-Xshareclasses:nonpersistent"
 fi
 benchmarks="$($java_path -jar $benchmark_path -l)"
 blist=(`echo ${benchmarks}`)
 for i in ${blist[@]};
 do 
 	echo $i >> $output_file;
+	$($java_path -Xshareclasses:destroyAll);
 	for j in `seq 0 10`;
 	do
-		time=$({ time $($java_path -jar $benchmark_path $i $aot  &> /dev/null) ; } 2>&1);
+		time=$({ time $($java_path $aot -jar $benchmark_path $i &> /dev/null) ; } 2>&1);
 		echo 'time: ' $time;
 		echo $time >> $output_file;
 	done
