926d6048c19f5e3f1775b5ef79b0cd992aede0e7
Leticia Wanderley <leticiafwanderley@gmail.com>
2018-11-27 03:28:45 +0000

    Update benchmarks script

--- benchmarks.sh
+++ benchmarks.sh
@@ -3,15 +3,19 @@ java_path=$1
 benchmark_path=$2
 output_file=$3
 aot=""
+is_aot=false
 if [ "$4" = "aot" ]; then
   aot="-Xshareclasses:nonpersistent"
+  is_aot=true
 fi
 benchmarks="$($java_path -jar $benchmark_path -l)"
 blist=(`echo ${benchmarks}`)
 for i in ${blist[@]};
 do 
 	echo $i >> $output_file;
-	$($java_path -Xshareclasses:destroyAll);
+	if $is_aot; then
+		$($java_path -Xshareclasses:destroyAll);
+	fi;
 	for j in `seq 0 10`;
 	do
 		time=$({ time $($java_path $aot -jar $benchmark_path $i &> /dev/null) ; } 2>&1);
