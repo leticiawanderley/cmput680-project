e5235f39af0ed23befac3f8290eb1bd998bda90d
Leticia Wanderley <leticiafwanderley@gmail.com>
2018-12-13 22:46:14 -0700

    Add sleep time between executions

--- benchmarks.sh
+++ benchmarks.sh
@@ -6,40 +6,40 @@ aot=""
 is_aot=false
 
 if [ "$4" = "aot" ]; then
-  aot="-Xshareclasses:nonpersistent"
   is_aot=true
 fi
 benchmarks="$($java_path -jar $benchmark_path -l)"
 blist=(`echo ${benchmarks}`)
-$failing_benchmarks="batik tomcat tradebeans tradesoap"
+failing_benchmarks="batik tomcat tradebeans tradesoap"
 
-benchmarks_list=()
+declare -a benchmarks_list=()
 for b in ${blist[@]};
-	do 
-		for failing_bechmark in $failing_benchmarks;
+	do
+		is_not_failing=true;
+		for failing_bechmark in ${failing_benchmarks[@]};
 		do
-			is_failing=false;
-			if ["$b" == "$failing_bechmark"]; then
-				is_failing=true;
+			if [ "${b}" == "${failing_bechmark}" ]; then
+				is_not_failing=false;
 			fi;
 	done
-	if $is_failing; then
-		benchmarks_list+=("$b");
+	if $is_not_failing; then
+		benchmarks_list+=("${b}");
 	fi;
 done
-		
-echo $benchmarks_list;
-
-for i in ${benchmarks_list[@]};
-do 
-	echo $i >> $output_file;
-	if $is_aot; then
-		$($java_path -Xshareclasses:destroyAll);
-	fi;
-	for j in `seq 0 20`;
-	do
+	
+for j in `seq 0 10`;
+do	
+	for i in ${benchmarks_list[@]};
+	do 
+		echo $i >> $output_file;
+		if $is_aot; then
+			aot=$($java_path -Xshareclasses:name=${i},verbose);
+			echo $aot;
+		fi;
 		time=$({ time $($java_path $aot -jar $benchmark_path $i &> /dev/null) ; } 2>&1);
 		echo 'time: ' $time;
 		echo $time >> $output_file;
+		sleep 1m;
 	done
+	sleep 12m;
 done
